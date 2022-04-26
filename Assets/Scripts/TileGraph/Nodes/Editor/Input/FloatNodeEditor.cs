db4693a12fef7a6650d70a7b1025643e26816c28
Stuart Lewis <b9050231@newcastle.ac.uk>
2022-04-26 08:57:13 +0100

    Fixed float input node

--- Assets/Scripts/TileGraph/Nodes/Editor/Input/FloatNodeEditor.cs
+++ Assets/Scripts/TileGraph/Nodes/Editor/Input/FloatNodeEditor.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 namespace TileGraph.Editors
 {
     [CustomNodeEditor(typeof(Nodes.FloatNode))]
-    public class FloatNodeEditor : BaseNodeEditor<Nodes.FloatNode>
+    public class FloatNodeEditor : InputNodeEditor<Nodes.FloatNode>
     {
-        private SerializedProperty _FloatOut;
-
-        protected override void OnNodeEnable()
-        {
-            _FloatOut = serializedObject.FindProperty("_FloatOut");
-        }
-
-        protected override void NodeInputGUI()
-        {
-            graph.editorUtilities.PortFieldMinLabel(_FloatOut);
-        }
-
-        protected override void NodeBodyGUI()
-        {
-            graph.editorUtilities.PropertyFieldMinLabel(_FloatOut, new GUIContent("value"));
-        }
     }
 }
--- Assets/Scripts/TileGraph/Nodes/Input/FloatNode.cs
+++ Assets/Scripts/TileGraph/Nodes/Input/FloatNode.cs
@@ -7,18 +7,9 @@ namespace TileGraph.Nodes
     [CreateNodeMenu("Input/Float", 2)]
     public class FloatNode : InputNode<float>
     {
-        [SerializeField, Output] private float _FloatOut = 0f;
-        
         private void Reset()
         {
             name = "Float";
-        }
-
-        public override object GetValue(NodePort port)
-        {
-            if (port.fieldName == "_FloatOut")
-                return _FloatOut;
-            return null;
         }
     }
 }
